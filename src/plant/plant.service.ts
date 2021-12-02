7d695b9ac56cb81acee006c85094acce5450aa44
Isabella Nunes <isabelladefreitasnunes@gmail.com>
2021-12-02 16:42:57 -0300

    Added createPlant method to PlantService

--- src/plant/plant.service.ts
+++ src/plant/plant.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PlantService {}
+export class PlantService {
+  banco = [];
+
+  createPlant(data) {
+    this.banco.push(data);
+    return this.banco;
+  }
+}
