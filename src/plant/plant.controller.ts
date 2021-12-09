fef5c651bb459c6f74ac6a8a779642551ddf3b1b
Isabella Nunes <isabelladefreitasnunes@gmail.com>
2021-12-09 00:54:09 -0300

    Added jwt token generator

--- src/plant/plant.controller.ts
+++ src/plant/plant.controller.ts
@@ -6,31 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PlantService } from './plant.service';
 import { CreatePlantDto } from './dto/create-plant.dto';
 import { UpdatePlantDto } from './dto/update-plant.dto';
 import { Plant } from '@prisma/client';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('plant')
 export class PlantController {
   constructor(private readonly plantService: PlantService) {}
 
+  @UseGuards(AuthGuard())
   @Post('create')
   create(@Body() createPlantDto: CreatePlantDto): Promise<Plant> {
     return this.plantService.create(createPlantDto);
   }
 
+  @UseGuards(AuthGuard())
   @Get('get-all')
   findAll(): Promise<Plant[]> {
     return this.plantService.findAll();
   }
 
+  @UseGuards(AuthGuard())
   @Get('find-one/:id')
   findOne(@Param('id') id: string): Promise<Plant> {
     return this.plantService.findOne(id);
   }
 
+  @UseGuards(AuthGuard())
   @Patch('update/:id')
   update(
     @Param('id') id: string,
@@ -39,6 +45,7 @@ export class PlantController {
     return this.plantService.update(id, updatePlantDto);
   }
 
+  @UseGuards(AuthGuard())
   @Delete('delete/:id')
   remove(@Param('id') id: string): Promise<{ message: string }> {
     return this.plantService.remove(id);
--- src/plant/plant.module.ts
+++ src/plant/plant.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlantService } from './plant.service';
 import { PlantController } from './plant.controller';
 import { PrismaService } from 'src/prisma.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [PlantController],
   providers: [PlantService, PrismaService],
 })
